86d6149e48639ec754345c4c5d7b54c88c3dd1bd
ankurax <53851825+ankurax@users.noreply.github.com>
2026-06-16 13:56:19 +0530

    1

---
+++ script.js
@@ -0,0 +1,26 @@
+function checkPassword() {
+
+    const password =
+        document.getElementById("password-input").value;
+
+    const correctPassword =
+        "mahadev071225";
+
+    if (password === correctPassword) {
+
+        document.getElementById(
+            "password-screen"
+        ).style.display = "none";
+
+        document.getElementById(
+            "main-content"
+        ).style.display = "block";
+
+    } else {
+
+        document.getElementById(
+            "error-message"
+        ).textContent =
+            "Incorrect password";
+    }
+}
