8ecde1dfb6fb42986f5c6ade326b1b846736eee3
ankurax <53851825+ankurax@users.noreply.github.com>
2026-06-18 16:30:10 +0530

    push

--- script.js
+++ script.js
@@ -47,6 +47,8 @@ function startStory() {
         .style.display = "flex";
 
     cinematicStarted = true;
+
+    updateCounter();
 }
 
 document.addEventListener("click", function(event) {
@@ -81,3 +83,98 @@ document.addEventListener("click", function(event) {
     scenes[currentScene]
         .classList.add("active");
 });
+
+document.addEventListener("click", function(event) {
+
+    if (!cinematicStarted)
+        return;
+
+    if (event.target.id === "openBtn")
+        return;
+
+    nextScene();
+});
+
+document.addEventListener("keydown", function(e) {
+
+    if (!cinematicStarted)
+        return;
+
+    if (e.key === "ArrowRight")
+        nextScene();
+
+    if (e.key === "ArrowLeft")
+        previousScene();
+});
+
+function updateCounter() {
+
+    document.getElementById("sceneCounter")
+        .textContent =
+            `${currentScene + 1} / ${scenes.length}`;
+}
+
+function nextScene() {
+
+    if (currentScene >= scenes.length - 1)
+        return;
+
+    scenes[currentScene]
+        .classList.remove("active");
+
+    currentScene++;
+
+    scenes[currentScene]
+        .classList.add("active");
+
+    updateCounter();
+}
+
+function previousScene() {
+
+    if (currentScene <= 0)
+        return;
+
+    scenes[currentScene]
+        .classList.remove("active");
+
+    currentScene--;
+
+    scenes[currentScene]
+        .classList.add("active");
+
+    updateCounter();
+}
+
+let touchStartX = 0;
+let touchEndX = 0;
+
+document.addEventListener("touchstart", function(e) {
+
+    touchStartX =
+        e.changedTouches[0].screenX;
+});
+
+document.addEventListener("touchend", function(e) {
+
+    touchEndX =
+        e.changedTouches[0].screenX;
+
+    handleSwipe();
+});
+
+function handleSwipe() {
+
+    const swipeDistance =
+        touchEndX - touchStartX;
+
+    if (swipeDistance > 60) {
+
+        previousScene();
+    }
+
+    if (swipeDistance < -60) {
+
+        nextScene();
+    }
+}
